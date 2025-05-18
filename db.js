88407fbf1d100b053fd2eebd852ee50e5d6ccf59
Caio Wesley <g8pro.rede@gmail.com>
2025-05-18 11:59:24 -0300

    Usa variáveis de ambiente na conexão com o banco

--- db.js
+++ db.js
@@ -1,13 +1,9 @@
-const mysql = require('mysql2');
-
 const pool = mysql.createPool({
-  host: 'srv1896.hstgr.io',
-  user: 'u167429742_adminreune',
-  password: '270389Caw@',
-  database: 'u167429742_gruporeune_db',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
-
-module.exports = pool;
